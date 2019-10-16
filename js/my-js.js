70eafd00f121a0d46fb82438e6c254eef9eb4556
KrivosheevAA <54209919+KrivosheevAA@users.noreply.github.com>
2019-10-17 00:22:58 +0300

    реализация js

--- js/my-js.js
+++ js/my-js.js
@@ -1,8 +1,38 @@
 var link = document.querySelector(".callback-popup");
 var popup = document.querySelector(".callback");
-
+var close = popup.querySelector(".close");
+var login = popup.querySelector("[name=fio]");
+var form = popup.querySelector("callback-form");
+var password = popup.querySelector("[name=e-mail]");
   
   link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("content-show");
-   });
+    login.focus();
+  });
+   
+  
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("content-show");
+    popup.classList.remove("modal-error");
+  });
+  
+
+   form.addEventListener("submit", function (evt) {
+    if (!login.value || !password.value) {
+      evt.preventDefault();
+      popup.classList.add("modal-error");
+    }
+  });
+
+  
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("content-show")) {
+        popup.classList.remove("content-show");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
