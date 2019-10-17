96753f761d4ea76f49f48d691f3632c61f3d4c77
KrivosheevAA <54209919+KrivosheevAA@users.noreply.github.com>
2019-10-18 00:30:48 +0300

    правки перед сдачей

--- js/my-js.js
+++ js/my-js.js
@@ -2,34 +2,37 @@ var link = document.querySelector(".callback-popup");
 var popup = document.querySelector(".callback");
 var close = popup.querySelector(".close");
 var login = popup.querySelector("[name=fio]");
-var form = popup.querySelector("callback-form");
+var form = popup.querySelector(".callback-form");
 var password = popup.querySelector("[name=e-mail]");
   
-  link.addEventListener("click", function (evt) {
-    evt.preventDefault();
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
     popup.classList.add("content-show");
     login.focus();
   });
    
   
-  close.addEventListener("click", function (evt) {
-    evt.preventDefault();
+  close.addEventListener("click", function (event) {
+    event.preventDefault();
     popup.classList.remove("content-show");
     popup.classList.remove("modal-error");
   });
   
 
-   form.addEventListener("submit", function (evt) {
+   form.addEventListener("submit", function (event) {
     if (!login.value || !password.value) {
-      evt.preventDefault();
+    	 event.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
       popup.classList.add("modal-error");
     }
   });
 
+
   
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
+  window.addEventListener("keydown", function (event) {
+    if (event.keyCode === 27) {
+      event.preventDefault();
       if (popup.classList.contains("content-show")) {
         popup.classList.remove("content-show");
         popup.classList.remove("modal-error");
