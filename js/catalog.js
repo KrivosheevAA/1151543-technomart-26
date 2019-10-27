3ebb9bcb110ad60526b017891b31dbaefa005a9c
KrivosheevAA <54209919+KrivosheevAA@users.noreply.github.com>
2019-10-27 19:54:09 +0300

    правки кода js

--- js/catalog.js
+++ js/catalog.js
@@ -28,3 +28,17 @@ for (var i = 0; i < cataloglink.length; i++) cataloglink[i].addEventListener("cl
       }
     }
   });
+
+var cataloginbookmarks = document.querySelectorAll(".in-bookmarks");
+var catalogbookmark = document.querySelector(".bookmark");
+var catalogbuy = document.querySelectorAll(".buy");
+var catalogbasket = document.querySelector(".basket");
+
+
+for (var i = 0; i < cataloginbookmarks.length; i++) cataloginbookmarks[i].addEventListener("click", function(event) {
+    event.preventDefault(), catalogbookmark.classList.add("added")
+});
+
+for (var i = 0; i < catalogbuy.length; i++) catalogbuy[i].addEventListener("click", function(event) {
+    event.preventDefault(), catalogbasket.classList.add("added")
+});
